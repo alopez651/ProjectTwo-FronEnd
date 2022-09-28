ed9670bfd5aef1dea89cbac70fd482dcde58fe04
alopez651 <ana.lopez6517@gmail.com>
2022-09-27 20:20:32 -0700

    adding files

--- projectTwo-app/src/app/added/added.component.ts
+++ projectTwo-app/src/app/added/added.component.ts
@@ -18,20 +18,28 @@ export class AddedComponent implements OnInit {
 
   ngOnInit(): void {
      let userId = Number(this.cookieService.get("userId"));
-    //let userId = Number(this.sessionService.get("userId"));
-    console.log(userId);
-
-    if(isNaN(userId)){
-      alert("You must be logged in to proceed! ");
-      this.router.navigate(["/products"]);
-    } 
-    else {
-      //this.usersService.getMovies(userId).subscribe(
       this.usersService.getMovies(userId).subscribe(
         returnVal => {
           this.added = returnVal;
         }
       )
-    }
   }
 }
+
+// let userId = Number(this.cookieService.get("userId"));
+//     //let userId = Number(this.sessionService.get("userId"));
+//     // console.log(userId);
+
+//     // if(isNaN(userId)){
+//     //   alert("You must be logged in to proceed! ");
+//     //   this.router.navigate(["/products"]);
+//     // } 
+//     // else {
+//       //this.usersService.getMovies(userId).subscribe(
+//       this.usersService.getMovies(userId).subscribe(
+//         returnVal => {
+//           this.added = returnVal;
+//         }
+//       )
+//     //}
+//   }
--- projectTwo-app/src/app/logout/logout.component.ts
+++ projectTwo-app/src/app/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookieService:CookieService, private router:Router) { }
 
   ngOnInit(): void {
+    this.cookieService.removeAll();
+    //this.router.navigate(["/products"]); 
   }
 
 }
